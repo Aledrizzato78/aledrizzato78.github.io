8fbcb60ae61453d7e96b337c52d748e0d6bbfd12
Aledrizzato78 <aledrizzato@gmail.com>
2025-06-18 11:44:32 -0300

    Atualiza BACKEND_URL com novo túnel: https://contributors-increases-vcr-tube.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Arquivo gerado automaticamente em Wed Jun 18 11:42:08     2025
-window.BACKEND_URL = 'https://phillips-florists-favourites-certificate.trycloudflare.com';
+// Arquivo gerado automaticamente em Wed Jun 18 11:44:32     2025
+window.BACKEND_URL = 'https://contributors-increases-vcr-tube.trycloudflare.com';
