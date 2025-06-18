fad493b3252e5ca5d3f1025cd9a59eb6ccdc90d7
Aledrizzato78 <aledrizzato@gmail.com>
2025-06-18 07:55:41 -0300

    Atualiza BACKEND_URL com novo túnel: https://compliant-able-sagem-conferences.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Arquivo gerado automaticamente em Tue Jun 17 09:48:26     2025
-window.BACKEND_URL = 'https://watershed-int-flows-hopkins.trycloudflare.com';
+// Arquivo gerado automaticamente em Wed Jun 18 07:55:41     2025
+window.BACKEND_URL = 'https://compliant-able-sagem-conferences.trycloudflare.com';
