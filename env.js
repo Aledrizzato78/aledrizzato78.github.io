049f4cbf554334b61a0e8ffeb5d2f7fd148a3c60
Aledrizzato78 <aledrizzato@gmail.com>
2025-06-18 11:22:06 -0300

    Atualiza BACKEND_URL com novo túnel: https://acrylic-toilet-said-manager.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Arquivo gerado automaticamente em Wed Jun 18 07:55:41     2025
-window.BACKEND_URL = 'https://compliant-able-sagem-conferences.trycloudflare.com';
+// Arquivo gerado automaticamente em Wed Jun 18 11:22:06     2025
+window.BACKEND_URL = 'https://acrylic-toilet-said-manager.trycloudflare.com';
