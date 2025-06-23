7421d399f941a23cd01928b9c0732b5ec8099333
Aledrizzato78 <aledrizzato@gmail.com>
2025-06-23 15:25:37 -0300

    Atualiza BACKEND_URL com novo túnel: https://promptly-lit-choice-beaches.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Arquivo gerado automaticamente em Mon Jun 23 08:09:58     2025
-window.BACKEND_URL = 'https://carries-many-shades-alphabetical.trycloudflare.com';
+// Arquivo gerado automaticamente em Mon Jun 23 15:25:36     2025
+window.BACKEND_URL = 'https://promptly-lit-choice-beaches.trycloudflare.com';
