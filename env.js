17778fa51959339503755c990c99bc8ccc3c02b8
Aledrizzato78 <aledrizzato@gmail.com>
2025-06-24 09:12:22 -0300

    Atualiza BACKEND_URL com novo túnel: https://calm-annex-disorder-johnson.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Arquivo gerado automaticamente em Mon Jun 23 15:25:36     2025
-window.BACKEND_URL = 'https://promptly-lit-choice-beaches.trycloudflare.com';
+// Arquivo gerado automaticamente em Tue Jun 24 09:12:22     2025
+window.BACKEND_URL = 'https://calm-annex-disorder-johnson.trycloudflare.com';
