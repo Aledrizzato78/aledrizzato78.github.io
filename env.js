0163d17f1fe5a1a3f83ac90f916a943f8c8e9e0f
Aledrizzato78 <aledrizzato@gmail.com>
2025-07-01 08:51:25 -0300

    Atualiza BACKEND_URL com novo túnel: https://treo-bed-author-analysis.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Arquivo gerado automaticamente em Tue Jul  1 08:22:09     2025
-window.BACKEND_URL = 'https://king-suite-societies-vulnerability.trycloudflare.com';
+// Arquivo gerado automaticamente em Tue Jul  1 08:51:24     2025
+window.BACKEND_URL = 'https://treo-bed-author-analysis.trycloudflare.com';
