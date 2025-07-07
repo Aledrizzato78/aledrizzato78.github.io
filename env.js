f53d338f95ee8bd4815375ca907d17f75d38a4ac
Aledrizzato78 <aledrizzato@gmail.com>
2025-07-07 08:02:12 -0300

    Atualiza BACKEND_URL com novo túnel: https://seen-cheese-checklist-contributor.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Arquivo gerado automaticamente em Fri Jul  4 08:10:28     2025
-window.BACKEND_URL = 'https://locking-reduced-crazy-british.trycloudflare.com';
+// Arquivo gerado automaticamente em Mon Jul  7 08:02:11     2025
+window.BACKEND_URL = 'https://seen-cheese-checklist-contributor.trycloudflare.com';
