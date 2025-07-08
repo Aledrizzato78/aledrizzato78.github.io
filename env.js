8e01877500f1c72cb01c971a9600f91e43ea99d8
Aledrizzato78 <aledrizzato@gmail.com>
2025-07-08 07:39:16 -0300

    Atualiza BACKEND_URL com novo túnel: https://fragrance-mortgage-temperature-redeem.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Arquivo gerado automaticamente em Tue Jul  8 07:38:25     2025
-window.BACKEND_URL = 'https://exactly-thermal-mongolia-blake.trycloudflare.com';
+// Arquivo gerado automaticamente em Tue Jul  8 07:39:16     2025
+window.BACKEND_URL = 'https://fragrance-mortgage-temperature-redeem.trycloudflare.com';
