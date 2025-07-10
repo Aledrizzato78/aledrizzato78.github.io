92bc42d5a3f20f5814d05511e38dd98535b63cc1
Aledrizzato78 <aledrizzato@gmail.com>
2025-07-10 08:24:19 -0300

    Atualiza BACKEND_URL para https://era-fan-advisor-improvements.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2025-07-10 08:23:37
-window.BACKEND_URL = 'https://picnic-wy-viewers-skating.trycloudflare.com'
+// Gerado automaticamente em 2025-07-10 08:24:19
+window.BACKEND_URL = 'https://era-fan-advisor-improvements.trycloudflare.com'
