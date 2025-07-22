c1f7ec6c1166237904fed9db7ef742eeaa056fa0
Aledrizzato78 <aledrizzato@gmail.com>
2025-07-22 07:56:07 -0300

    Atualiza BACKEND_URL para https://hip-carnival-hard-sep.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2025-07-21 08:27:30
-window.BACKEND_URL = 'https://ct-reduced-and-theta.trycloudflare.com'
+// Gerado automaticamente em 2025-07-22 07:56:07
+window.BACKEND_URL = 'https://hip-carnival-hard-sep.trycloudflare.com'
