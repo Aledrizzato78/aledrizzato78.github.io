2f4317f63948bdb6effa7b5c1d26e5b65aa1c458
Aledrizzato78 <aledrizzato@gmail.com>
2025-07-28 14:50:14 -0300

    Atualiza BACKEND_URL para https://batteries-christina-afraid-talks.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2025-07-28 09:12:45
-window.BACKEND_URL = 'https://pod-islamic-stomach-childhood.trycloudflare.com'
+// Gerado automaticamente em 2025-07-28 14:50:14
+window.BACKEND_URL = 'https://batteries-christina-afraid-talks.trycloudflare.com'
