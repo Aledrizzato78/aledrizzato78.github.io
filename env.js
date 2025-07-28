0494528a1e8a7551cd19e0fd835cd118f1d034b0
Aledrizzato78 <aledrizzato@gmail.com>
2025-07-28 09:12:45 -0300

    Atualiza BACKEND_URL para https://pod-islamic-stomach-childhood.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2025-07-28 07:56:47
-window.BACKEND_URL = 'https://suppose-concept-blow-carriers.trycloudflare.com'
+// Gerado automaticamente em 2025-07-28 09:12:45
+window.BACKEND_URL = 'https://pod-islamic-stomach-childhood.trycloudflare.com'
