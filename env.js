9475d6da86298fc2d4c59f057cfe3353182d154d
Aledrizzato78 <aledrizzato@gmail.com>
2025-07-29 15:52:30 -0300

    Atualiza BACKEND_URL para https://wooden-sleeps-data-hoping.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2025-07-29 15:47:26
-window.BACKEND_URL = 'https://collapse-pennsylvania-gb-speeches.trycloudflare.com'
+// Gerado automaticamente em 2025-07-29 15:52:30
+window.BACKEND_URL = 'https://wooden-sleeps-data-hoping.trycloudflare.com'
