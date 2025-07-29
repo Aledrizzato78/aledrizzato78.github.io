9478054ffa72822f390c3d4c272bdb9a754c342b
Aledrizzato78 <aledrizzato@gmail.com>
2025-07-29 16:05:23 -0300

    Atualiza BACKEND_URL para https://retirement-gear-hose-virtual.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2025-07-29 15:52:30
-window.BACKEND_URL = 'https://wooden-sleeps-data-hoping.trycloudflare.com'
+// Gerado automaticamente em 2025-07-29 16:05:23
+window.BACKEND_URL = 'https://retirement-gear-hose-virtual.trycloudflare.com'
