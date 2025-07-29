4ebf64a0a9878a474a1f2bbe0be24e03b080373e
Aledrizzato78 <aledrizzato@gmail.com>
2025-07-29 12:17:01 -0300

    Atualiza BACKEND_URL para https://menu-redhead-maintenance-pending.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2025-07-29 12:10:11
-window.BACKEND_URL = 'https://hop-inquiry-annually-.trycloudflare.com'
+// Gerado automaticamente em 2025-07-29 12:17:01
+window.BACKEND_URL = 'https://menu-redhead-maintenance-pending.trycloudflare.com'
