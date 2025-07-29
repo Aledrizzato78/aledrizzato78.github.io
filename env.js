ea04f215b294aff1bfc4981c12fa8c3fb4701bfc
Aledrizzato78 <aledrizzato@gmail.com>
2025-07-29 10:38:17 -0300

    Atualiza BACKEND_URL para https://ferry-actually-carrying-legacy.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2025-07-29 09:42:46
-window.BACKEND_URL = 'https://tapes-ports-commercial-indie.trycloudflare.com'
+// Gerado automaticamente em 2025-07-29 10:38:16
+window.BACKEND_URL = 'https://ferry-actually-carrying-legacy.trycloudflare.com'
