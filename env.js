ebc67e1ee9aa8509f052fb1a401d1de4771339fa
Aledrizzato78 <aledrizzato@gmail.com>
2025-07-29 15:44:49 -0300

    Atualiza BACKEND_URL para https://predictions-palestinian-models-capital.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2025-07-29 15:42:17
-window.BACKEND_URL = 'https://pie-spelling-blood-connections.trycloudflare.com'
+// Gerado automaticamente em 2025-07-29 15:44:49
+window.BACKEND_URL = 'https://predictions-palestinian-models-capital.trycloudflare.com'
