52c4ffd9725d2dbaa3bbbf02fe0dba52a53a18af
Aledrizzato78 <aledrizzato@gmail.com>
2025-07-29 10:42:25 -0300

    Atualiza BACKEND_URL para https://scripting-nutritional-tables-extent.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2025-07-29 10:38:16
-window.BACKEND_URL = 'https://ferry-actually-carrying-legacy.trycloudflare.com'
+// Gerado automaticamente em 2025-07-29 10:42:25
+window.BACKEND_URL = 'https://scripting-nutritional-tables-extent.trycloudflare.com'
