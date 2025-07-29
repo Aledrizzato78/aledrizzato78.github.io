19c845881ce23e7d64d9436d4695d663b8f86840
Aledrizzato78 <aledrizzato@gmail.com>
2025-07-29 09:39:19 -0300

    Atualiza BACKEND_URL para https://athletes-investors-claimed-layout.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2025-07-29 09:17:02
-window.BACKEND_URL = 'https://erik-soma-xml-semi.trycloudflare.com'
+// Gerado automaticamente em 2025-07-29 09:39:19
+window.BACKEND_URL = 'https://athletes-investors-claimed-layout.trycloudflare.com'
