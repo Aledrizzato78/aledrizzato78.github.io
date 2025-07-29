aed88582e2e724c9b4e68c2220865ab7a58ba659
Aledrizzato78 <aledrizzato@gmail.com>
2025-07-29 15:11:07 -0300

    Atualiza BACKEND_URL para https://path-wallpaper-setting-ordering.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2025-07-29 12:17:01
-window.BACKEND_URL = 'https://menu-redhead-maintenance-pending.trycloudflare.com'
+// Gerado automaticamente em 2025-07-29 15:11:06
+window.BACKEND_URL = 'https://path-wallpaper-setting-ordering.trycloudflare.com'
