314c5dd31245e6e0f8806010e530d18feebb3b26
Aledrizzato78 <aledrizzato@gmail.com>
2025-07-29 12:10:11 -0300

    Atualiza BACKEND_URL para https://hop-inquiry-annually-.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2025-07-29 10:48:27
-window.BACKEND_URL = 'https://ballot-patrol-lt-committee.trycloudflare.com'
+// Gerado automaticamente em 2025-07-29 12:10:11
+window.BACKEND_URL = 'https://hop-inquiry-annually-.trycloudflare.com'
