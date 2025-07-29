0638e2a3e1c414422cba9e4e5ca2a2925b8d2fe0
Aledrizzato78 <aledrizzato@gmail.com>
2025-07-29 15:14:09 -0300

    Atualiza BACKEND_URL para https://without-k-barnes-comments.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2025-07-29 15:11:06
-window.BACKEND_URL = 'https://path-wallpaper-setting-ordering.trycloudflare.com'
+// Gerado automaticamente em 2025-07-29 15:14:09
+window.BACKEND_URL = 'https://without-k-barnes-comments.trycloudflare.com'
