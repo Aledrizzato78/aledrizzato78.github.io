8745747f82b96f06c6e7b8cd56e0cc85fbc4eaad
Aledrizzato78 <aledrizzato@gmail.com>
2025-07-29 08:04:54 -0300

    Atualiza BACKEND_URL para https://go-minerals-divisions-pieces.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2025-07-28 14:51:02
-window.BACKEND_URL = 'Binary file /c/Users/Latitude 5280/AppData/Local/Temp/cloudflared_quick.log matches'
+// Gerado automaticamente em 2025-07-29 08:04:53
+window.BACKEND_URL = 'https://go-minerals-divisions-pieces.trycloudflare.com'
