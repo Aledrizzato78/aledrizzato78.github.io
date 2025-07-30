6989bbf006d4b94fa0dbca42a8b39b4c94bac412
Aledrizzato78 <aledrizzato@gmail.com>
2025-07-30 11:11:10 -0300

    Atualiza BACKEND_URL para https://shades-certificate-occurring-reload.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2025-07-30 11:02:59
-window.BACKEND_URL = 'https://uncle-alpine-mandatory-walt.trycloudflare.com'
+// Gerado automaticamente em 2025-07-30 11:11:10
+window.BACKEND_URL = 'https://shades-certificate-occurring-reload.trycloudflare.com'
