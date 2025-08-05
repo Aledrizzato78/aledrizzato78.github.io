e0d5b62d24434c50597183a4fe8f91f3fea9c2cb
Aledrizzato78 <aledrizzato@gmail.com>
2025-08-05 07:37:03 -0300

    Atualiza BACKEND_URL para https://pretty-sms-make-gerald.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2025-08-04 07:58:12
-window.BACKEND_URL = 'https://whenever-exclude-m-resulted.trycloudflare.com'
+// Gerado automaticamente em 2025-08-05 07:37:03
+window.BACKEND_URL = 'https://pretty-sms-make-gerald.trycloudflare.com'
