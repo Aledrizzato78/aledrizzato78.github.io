5413fb7d0be061be60c5ab1700fa4579bc53dc54
Aledrizzato78 <aledrizzato@gmail.com>
2025-08-07 09:30:00 -0300

    Atualiza BACKEND_URL para https://thinking-comic-oregon-fell.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2025-08-07 07:45:41
-window.BACKEND_URL = 'https://holding-apps-cincinnati-compatible.trycloudflare.com'
+// Gerado automaticamente em 2025-08-07 09:30:00
+window.BACKEND_URL = 'https://thinking-comic-oregon-fell.trycloudflare.com'
