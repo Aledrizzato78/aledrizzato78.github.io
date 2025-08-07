9feee86448c49bbdf943cbe607cd7e7985b8f8e6
Aledrizzato78 <aledrizzato@gmail.com>
2025-08-07 09:30:48 -0300

    Atualiza BACKEND_URL para https://hosts-palm-upcoming-jar.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2025-08-07 09:30:00
-window.BACKEND_URL = 'https://thinking-comic-oregon-fell.trycloudflare.com'
+// Gerado automaticamente em 2025-08-07 09:30:47
+window.BACKEND_URL = 'https://hosts-palm-upcoming-jar.trycloudflare.com'
