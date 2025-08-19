4d842ba6061a5de1d773016d091bba252f31752a
Aledrizzato78 <aledrizzato@gmail.com>
2025-08-19 07:45:40 -0300

    Atualiza BACKEND_URL para https://cases-dpi-middle-universal.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2025-08-18 08:01:04
-window.BACKEND_URL = 'https://dad-cultures-excluded-convert.trycloudflare.com'
+// Gerado automaticamente em 2025-08-19 07:45:40
+window.BACKEND_URL = 'https://cases-dpi-middle-universal.trycloudflare.com'
