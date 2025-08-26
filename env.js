c1c0c2b2819a939dd0a0753080883a14204cf800
Aledrizzato78 <aledrizzato@gmail.com>
2025-08-26 07:34:29 -0300

    Atualiza BACKEND_URL para https://knitting-damaged-facilities-exclusion.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2025-08-25 07:59:53
-window.BACKEND_URL = 'https://scholar-partition-warnings-pentium.trycloudflare.com'
+// Gerado automaticamente em 2025-08-26 07:34:29
+window.BACKEND_URL = 'https://knitting-damaged-facilities-exclusion.trycloudflare.com'
