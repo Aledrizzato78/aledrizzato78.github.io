785b8b8181c76c2698e00317196c473504f5d872
Aledrizzato78 <aledrizzato@gmail.com>
2025-08-27 07:41:36 -0300

    Atualiza BACKEND_URL para https://rebound-excellence-opinion-assist.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2025-08-26 07:34:29
-window.BACKEND_URL = 'https://knitting-damaged-facilities-exclusion.trycloudflare.com'
+// Gerado automaticamente em 2025-08-27 07:41:35
+window.BACKEND_URL = 'https://rebound-excellence-opinion-assist.trycloudflare.com'
