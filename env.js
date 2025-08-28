629eee6720865150cda2a5831f4a6ebac3a46806
Aledrizzato78 <aledrizzato@gmail.com>
2025-08-28 09:18:13 -0300

    Atualiza BACKEND_URL para https://making-fri-victory-destiny.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2025-08-27 07:41:35
-window.BACKEND_URL = 'https://rebound-excellence-opinion-assist.trycloudflare.com'
+// Gerado automaticamente em 2025-08-28 09:18:12
+window.BACKEND_URL = 'https://making-fri-victory-destiny.trycloudflare.com'
