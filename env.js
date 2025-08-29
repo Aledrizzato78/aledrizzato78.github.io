c869ae910e989f068bcec5bd6fa45e9e0a421c52
Aledrizzato78 <aledrizzato@gmail.com>
2025-08-29 08:07:00 -0300

    Atualiza BACKEND_URL para https://chad-solutions-precious-classroom.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2025-08-28 09:18:12
-window.BACKEND_URL = 'https://making-fri-victory-destiny.trycloudflare.com'
+// Gerado automaticamente em 2025-08-29 08:07:00
+window.BACKEND_URL = 'https://chad-solutions-precious-classroom.trycloudflare.com'
