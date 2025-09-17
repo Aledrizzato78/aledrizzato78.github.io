d403813abf3baa9d6499c547af7ecf943d1e58fb
Aledrizzato78 <aledrizzato@gmail.com>
2025-09-17 08:45:59 -0300

    Atualiza BACKEND_URL para https://grows-prizes-alto-gear.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2025-08-29 08:07:00
-window.BACKEND_URL = 'https://chad-solutions-precious-classroom.trycloudflare.com'
+// Gerado automaticamente em 2025-09-17 08:45:58
+window.BACKEND_URL = 'https://grows-prizes-alto-gear.trycloudflare.com'
