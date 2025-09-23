dd3abceb6344fe52dfc252d76d42a6baf712fe88
Aledrizzato78 <aledrizzato@gmail.com>
2025-09-23 08:07:51 -0300

    Atualiza BACKEND_URL para https://stocks-optimum-indirect-dryer.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2025-09-22 07:18:39
-window.BACKEND_URL = 'https://stamps-shortly-chemical-metric.trycloudflare.com'
+// Gerado automaticamente em 2025-09-23 08:07:50
+window.BACKEND_URL = 'https://stocks-optimum-indirect-dryer.trycloudflare.com'
