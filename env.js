a4fb8637d6f71d45844be37a14bbc33f14f5e6fe
Aledrizzato78 <aledrizzato@gmail.com>
2025-09-24 09:29:55 -0300

    Atualiza BACKEND_URL para https://spots-hay-applicant-baseline.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2025-09-23 08:07:50
-window.BACKEND_URL = 'https://stocks-optimum-indirect-dryer.trycloudflare.com'
+// Gerado automaticamente em 2025-09-24 09:29:55
+window.BACKEND_URL = 'https://spots-hay-applicant-baseline.trycloudflare.com'
