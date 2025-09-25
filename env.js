44424b26b899676c932c44d67b9f99eb007e5dc6
Aledrizzato78 <aledrizzato@gmail.com>
2025-09-25 08:03:58 -0300

    Atualiza BACKEND_URL para https://experiences-classical-vintage-attacked.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2025-09-24 09:29:55
-window.BACKEND_URL = 'https://spots-hay-applicant-baseline.trycloudflare.com'
+// Gerado automaticamente em 2025-09-25 08:03:57
+window.BACKEND_URL = 'https://experiences-classical-vintage-attacked.trycloudflare.com'
