a2c11a52de88536608b4e06e1e0ff3b107157454
Aledrizzato78 <aledrizzato@gmail.com>
2025-10-06 07:53:22 -0300

    Atualiza BACKEND_URL para https://expires-vote-cuts-concert.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2025-10-03 08:20:30
-window.BACKEND_URL = 'https://multiple-resolved-assisted-filtering.trycloudflare.com'
+// Gerado automaticamente em 2025-10-06 07:53:21
+window.BACKEND_URL = 'https://expires-vote-cuts-concert.trycloudflare.com'
