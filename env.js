c856e04d232852271bcd38f62198cbb960498ff8
Aledrizzato78 <aledrizzato@gmail.com>
2025-10-08 07:55:25 -0300

    Atualiza BACKEND_URL para https://total-viewer-buddy-strain.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2025-10-07 07:58:48
-window.BACKEND_URL = 'https://todd-methodology-paying-compression.trycloudflare.com'
+// Gerado automaticamente em 2025-10-08 07:55:25
+window.BACKEND_URL = 'https://total-viewer-buddy-strain.trycloudflare.com'
