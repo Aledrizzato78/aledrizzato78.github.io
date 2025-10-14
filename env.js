5a8300f5be3c7278b929116a92a543e0cb43585d
Aledrizzato78 <aledrizzato@gmail.com>
2025-10-14 09:35:51 -0300

    Atualiza BACKEND_URL para https://bow-bidder-ashley-farming.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2025-10-13 05:58:06
-window.BACKEND_URL = 'https://letters-attorneys-vendors-featured.trycloudflare.com'
+// Gerado automaticamente em 2025-10-14 09:35:51
+window.BACKEND_URL = 'https://bow-bidder-ashley-farming.trycloudflare.com'
