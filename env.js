15deff29fc4e47ce678008ab6c528f165ca1d9a3
Aledrizzato78 <aledrizzato@gmail.com>
2025-10-15 08:56:55 -0300

    Atualiza BACKEND_URL para https://agree-reducing-ids-constantly.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2025-10-14 09:35:51
-window.BACKEND_URL = 'https://bow-bidder-ashley-farming.trycloudflare.com'
+// Gerado automaticamente em 2025-10-15 08:56:54
+window.BACKEND_URL = 'https://agree-reducing-ids-constantly.trycloudflare.com'
