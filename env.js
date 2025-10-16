37e99c5645ac324dac27dedecb082276e98faeda
Aledrizzato78 <aledrizzato@gmail.com>
2025-10-16 07:04:28 -0300

    Atualiza BACKEND_URL para https://repeated-instructional-align-placed.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2025-10-15 08:56:54
-window.BACKEND_URL = 'https://agree-reducing-ids-constantly.trycloudflare.com'
+// Gerado automaticamente em 2025-10-16 07:04:28
+window.BACKEND_URL = 'https://repeated-instructional-align-placed.trycloudflare.com'
