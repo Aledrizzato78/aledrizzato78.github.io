642ad7956832ef04fbad9389eaa24fac75aef7dd
Aledrizzato78 <aledrizzato@gmail.com>
2025-10-17 10:57:52 -0300

    Atualiza BACKEND_URL para https://affordable-cedar-testimonials-multiple.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2025-10-16 07:04:28
-window.BACKEND_URL = 'https://repeated-instructional-align-placed.trycloudflare.com'
+// Gerado automaticamente em 2025-10-17 10:57:51
+window.BACKEND_URL = 'https://affordable-cedar-testimonials-multiple.trycloudflare.com'
