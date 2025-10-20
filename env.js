7ff0343bc152eeeef54487647b4fc57cf5bdc021
Aledrizzato78 <aledrizzato@gmail.com>
2025-10-20 07:56:11 -0300

    Atualiza BACKEND_URL para https://minds-save-solution-alone.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2025-10-17 10:57:51
-window.BACKEND_URL = 'https://affordable-cedar-testimonials-multiple.trycloudflare.com'
+// Gerado automaticamente em 2025-10-20 07:56:11
+window.BACKEND_URL = 'https://minds-save-solution-alone.trycloudflare.com'
