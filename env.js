9af990cb3a8e479978c1c84199840f287fd58ce0
Aledrizzato78 <aledrizzato@gmail.com>
2025-10-27 06:43:56 -0300

    Atualiza BACKEND_URL para https://necklace-inspections-teacher-ribbon.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2025-10-22 08:20:35
-window.BACKEND_URL = 'https://acids-pros-seems-hay.trycloudflare.com'
+// Gerado automaticamente em 2025-10-27 06:43:55
+window.BACKEND_URL = 'https://necklace-inspections-teacher-ribbon.trycloudflare.com'
