504ae50dbc38c43431d6d3bc0509d66f46ec8603
Aledrizzato78 <aledrizzato@gmail.com>
2025-10-27 09:08:23 -0300

    Atualiza BACKEND_URL para https://raise-africa-tank-character.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2025-10-27 09:03:58
-window.BACKEND_URL = 'https://link-avi-absorption-sugar.trycloudflare.com'
+// Gerado automaticamente em 2025-10-27 09:08:23
+window.BACKEND_URL = 'https://raise-africa-tank-character.trycloudflare.com'
