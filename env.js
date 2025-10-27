b85294be2d5f788790a11c277674e48b54116e3c
Aledrizzato78 <aledrizzato@gmail.com>
2025-10-27 08:54:05 -0300

    Atualiza BACKEND_URL para https://quebec-second-hammer-banner.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2025-10-27 08:39:25
-window.BACKEND_URL = 'https://generated-sparc-naples-hope.trycloudflare.com'
+// Gerado automaticamente em 2025-10-27 08:54:05
+window.BACKEND_URL = 'https://quebec-second-hammer-banner.trycloudflare.com'
