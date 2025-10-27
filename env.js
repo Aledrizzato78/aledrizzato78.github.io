7e2e94209553e9b823f98de905afbec33ff1a4f0
Aledrizzato78 <aledrizzato@gmail.com>
2025-10-27 08:39:26 -0300

    Atualiza BACKEND_URL para https://generated-sparc-naples-hope.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2025-10-27 08:38:05
-window.BACKEND_URL = 'https://menus-hart-hollywood-equality.trycloudflare.com'
+// Gerado automaticamente em 2025-10-27 08:39:25
+window.BACKEND_URL = 'https://generated-sparc-naples-hope.trycloudflare.com'
