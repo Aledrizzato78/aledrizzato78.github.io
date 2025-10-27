69dd4edf9dbe08565b6443b4bc77ee9be6a3e84e
Aledrizzato78 <aledrizzato@gmail.com>
2025-10-27 07:48:23 -0300

    Atualiza BACKEND_URL para https://negotiations-intellectual-contractors-took.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2025-10-27 07:42:49
-window.BACKEND_URL = 'https://fioricet-yen-dakota-yield.trycloudflare.com'
+// Gerado automaticamente em 2025-10-27 07:48:22
+window.BACKEND_URL = 'https://negotiations-intellectual-contractors-took.trycloudflare.com'
