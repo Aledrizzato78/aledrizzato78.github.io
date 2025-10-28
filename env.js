2ca502db05d036eaaa5fcf194736effae99db8ca
Aledrizzato78 <aledrizzato@gmail.com>
2025-10-28 08:56:10 -0300

    Atualizar URL da API para https://incentives-solely-logging-construct.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2025-10-27 11:22:59
-window.BACKEND_URL = 'https://organizer-sticky-horn-stretch.trycloudflare.com'
+// Gerado automaticamente em 28/10/2025  8:56:10,26 
+window.BACKEND_URL = 'https://https://incentives-solely-logging-construct.trycloudflare.com' 
