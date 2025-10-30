5eaecc9c02567358410a97f655062d6f6dd7e52e
Aledrizzato78 <aledrizzato@gmail.com>
2025-10-30 06:55:27 -0300

    Atualiza BACKEND_URL para https://arguments-controversy-race-books.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2025-10-29 06:29:25
-window.BACKEND_URL = 'https://contribute-logged-webpage-passing.trycloudflare.com'
+// Gerado automaticamente em 2025-10-30 06:55:27
+window.BACKEND_URL = 'https://arguments-controversy-race-books.trycloudflare.com'
