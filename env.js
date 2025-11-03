57360cedc5c2e5345fbf366d375c8096088d3f72
Aledrizzato78 <aledrizzato@gmail.com>
2025-11-03 06:26:15 -0300

    Atualiza BACKEND_URL para https://romantic-impression-ash-dennis.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2025-10-31 08:53:56
-window.BACKEND_URL = 'https://star-advocate-machines-tub.trycloudflare.com'
+// Gerado automaticamente em 2025-11-03 06:26:14
+window.BACKEND_URL = 'https://romantic-impression-ash-dennis.trycloudflare.com'
