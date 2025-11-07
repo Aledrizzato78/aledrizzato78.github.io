704fd38ad5a88bd4c5dc76e327a68d7402a78783
Aledrizzato78 <aledrizzato@gmail.com>
2025-11-07 08:05:25 -0300

    Atualiza BACKEND_URL para https://invoice-blues-walks-claire.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2025-11-06 08:35:26
-window.BACKEND_URL = 'https://academics-direction-rats-offering.trycloudflare.com'
+// Gerado automaticamente em 2025-11-07 08:05:23
+window.BACKEND_URL = 'https://invoice-blues-walks-claire.trycloudflare.com'
