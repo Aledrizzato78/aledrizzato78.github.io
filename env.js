53c33ba2f9d395733b48e07c051086fbc012cada
Aledrizzato78 <aledrizzato@gmail.com>
2025-11-10 08:28:31 -0300

    Atualiza BACKEND_URL para https://tea-study-cells-starring.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2025-11-07 08:05:23
-window.BACKEND_URL = 'https://invoice-blues-walks-claire.trycloudflare.com'
+// Gerado automaticamente em 2025-11-10 08:28:31
+window.BACKEND_URL = 'https://tea-study-cells-starring.trycloudflare.com'
