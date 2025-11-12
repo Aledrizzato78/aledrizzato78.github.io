994b443e34677167e257f0db870b709cf4f8038d
Aledrizzato78 <aledrizzato@gmail.com>
2025-11-12 08:33:51 -0300

    Atualiza BACKEND_URL para https://consist-authorities-detection-expertise.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2025-11-12 08:33:21
-window.BACKEND_URL = 'https://feeling-partnerships-henderson-nuke.trycloudflare.com'
+// Gerado automaticamente em 2025-11-12 08:33:51
+window.BACKEND_URL = 'https://consist-authorities-detection-expertise.trycloudflare.com'
