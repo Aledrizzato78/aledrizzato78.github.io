95ebd88b75649832484caac7b934449f3b276bcc
Aledrizzato78 <aledrizzato@gmail.com>
2025-11-12 08:33:21 -0300

    Atualiza BACKEND_URL para https://feeling-partnerships-henderson-nuke.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2025-11-11 06:21:47
-window.BACKEND_URL = 'https://doc-dense-sprint-warrior.trycloudflare.com'
+// Gerado automaticamente em 2025-11-12 08:33:21
+window.BACKEND_URL = 'https://feeling-partnerships-henderson-nuke.trycloudflare.com'
