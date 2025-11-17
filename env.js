14558e3b5a2168311a7719fd8d247b413a3daba5
Aledrizzato78 <aledrizzato@gmail.com>
2025-11-17 06:20:51 -0300

    Atualiza BACKEND_URL para https://morrison-temperature-presentation-closest.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2025-11-13 05:45:38
-window.BACKEND_URL = 'https://gem-cigarette-percentage-has.trycloudflare.com'
+// Gerado automaticamente em 2025-11-17 06:20:49
+window.BACKEND_URL = 'https://morrison-temperature-presentation-closest.trycloudflare.com'
