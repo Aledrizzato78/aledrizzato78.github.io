068dd931e87fc1b4581f0ef28c894b911626239a
Aledrizzato78 <aledrizzato@gmail.com>
2025-11-18 11:41:23 -0300

    Atualiza BACKEND_URL para https://maps-beyond-beautiful-marketing.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2025-11-18 10:56:33
-window.BACKEND_URL = 'https://marvel-continue-richard-approaches.trycloudflare.com'
+// Gerado automaticamente em 2025-11-18 11:41:23
+window.BACKEND_URL = 'https://maps-beyond-beautiful-marketing.trycloudflare.com'
