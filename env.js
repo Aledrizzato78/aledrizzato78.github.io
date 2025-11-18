132ec99ff0f0bb91d550e359b56c4a8a6cb62756
Aledrizzato78 <aledrizzato@gmail.com>
2025-11-18 10:38:40 -0300

    Atualiza BACKEND_URL para https://direction-course-been-cute.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2025-11-18 07:33:04
-window.BACKEND_URL = 'https://promising-avi-barely-hybrid.trycloudflare.com'
+// Gerado automaticamente em 2025-11-18 10:38:39
+window.BACKEND_URL = 'https://direction-course-been-cute.trycloudflare.com'
