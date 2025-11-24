b21cbae2c6eb848506f476b46759ff94ae52ce78
Aledrizzato78 <aledrizzato@gmail.com>
2025-11-24 06:33:47 -0300

    Atualiza BACKEND_URL para https://broader-rejected-left-prince.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2025-11-18 11:41:23
-window.BACKEND_URL = 'https://maps-beyond-beautiful-marketing.trycloudflare.com'
+// Gerado automaticamente em 2025-11-24 06:33:46
+window.BACKEND_URL = 'https://broader-rejected-left-prince.trycloudflare.com'
