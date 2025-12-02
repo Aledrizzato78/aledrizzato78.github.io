550abaed0be3a3ff5aea02ffc4a914c5929f2bda
Aledrizzato78 <aledrizzato@gmail.com>
2025-12-02 06:53:45 -0300

    Atualiza BACKEND_URL para https://villa-her-gain-phrase.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2025-12-01 09:00:39
-window.BACKEND_URL = 'https://time-remained-rep-remarks.trycloudflare.com'
+// Gerado automaticamente em 2025-12-02 06:53:45
+window.BACKEND_URL = 'https://villa-her-gain-phrase.trycloudflare.com'
