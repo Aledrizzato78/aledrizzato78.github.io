57c788cbd070a9ae1058c339d9705bad7e7943ed
Aledrizzato78 <aledrizzato@gmail.com>
2025-12-15 06:12:31 -0300

    Atualiza BACKEND_URL para https://delete-linux-maternity-deaths.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2025-12-11 08:39:32
-window.BACKEND_URL = 'https://cliff-snowboard-heat-survey.trycloudflare.com'
+// Gerado automaticamente em 2025-12-15 06:12:30
+window.BACKEND_URL = 'https://delete-linux-maternity-deaths.trycloudflare.com'
