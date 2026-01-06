6441d4c1e298d5882fb2bba385bc39d07610d876
Aledrizzato78 <aledrizzato@gmail.com>
2026-01-06 08:42:04 -0300

    Atualiza BACKEND_URL para https://dogs-bringing-hats-alice.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2026-01-05 06:50:28
-window.BACKEND_URL = 'https://carbon-pierce-despite-holidays.trycloudflare.com'
+// Gerado automaticamente em 2026-01-06 08:42:03
+window.BACKEND_URL = 'https://dogs-bringing-hats-alice.trycloudflare.com'
