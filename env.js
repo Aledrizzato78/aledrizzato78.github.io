428c98e3278d7a449d8a6e43ee21963047c64fb9
Aledrizzato78 <aledrizzato@gmail.com>
2026-01-08 08:14:48 -0300

    Atualiza BACKEND_URL para https://varies-finds-november-conference.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2026-01-08 07:50:13
-window.BACKEND_URL = 'https://flex-deemed-felt-helmet.trycloudflare.com'
+// Gerado automaticamente em 2026-01-08 08:14:48
+window.BACKEND_URL = 'https://varies-finds-november-conference.trycloudflare.com'
