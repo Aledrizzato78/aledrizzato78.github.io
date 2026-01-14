4ec3fefd9bd3bf3649f70d7c2d31757c911842b8
Aledrizzato78 <aledrizzato@gmail.com>
2026-01-14 08:58:42 -0300

    Atualiza BACKEND_URL para https://writers-locate-collection-aruba.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2026-01-13 07:24:07
-window.BACKEND_URL = 'https://streets-toner-generating-ink.trycloudflare.com'
+// Gerado automaticamente em 2026-01-14 08:58:42
+window.BACKEND_URL = 'https://writers-locate-collection-aruba.trycloudflare.com'
