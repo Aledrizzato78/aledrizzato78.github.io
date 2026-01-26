4b63d9f27d1546cb7356071ec03f978f88816c3c
Aledrizzato78 <aledrizzato@gmail.com>
2026-01-26 10:43:43 -0300

    Atualiza BACKEND_URL para https://faculty-whatever-stopping-parade.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2026-01-26 10:42:18
-window.BACKEND_URL = 'https://cbs-camps-vids-mails.trycloudflare.com'
+// Gerado automaticamente em 2026-01-26 10:43:43
+window.BACKEND_URL = 'https://faculty-whatever-stopping-parade.trycloudflare.com'
