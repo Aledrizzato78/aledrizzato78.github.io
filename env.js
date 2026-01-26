34ffbe10fc7f395be317531f55af479fd3f81a47
Aledrizzato78 <aledrizzato@gmail.com>
2026-01-26 08:59:04 -0300

    Atualiza BACKEND_URL para https://questionnaire-principle-apart-wherever.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2026-01-14 08:58:42
-window.BACKEND_URL = 'https://writers-locate-collection-aruba.trycloudflare.com'
+// Gerado automaticamente em 2026-01-26 08:59:03
+window.BACKEND_URL = 'https://questionnaire-principle-apart-wherever.trycloudflare.com'
