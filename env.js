9ce3bc819d9973d6389e4c1a8330786f3e1c9661
Aledrizzato78 <aledrizzato@gmail.com>
2026-01-26 10:31:31 -0300

    Atualiza BACKEND_URL para https://shadows-members-vocal-lead.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2026-01-26 10:26:55
-window.BACKEND_URL = 'https://engineer-its-runs-inclusion.trycloudflare.com'
+// Gerado automaticamente em 2026-01-26 10:31:31
+window.BACKEND_URL = 'https://shadows-members-vocal-lead.trycloudflare.com'
