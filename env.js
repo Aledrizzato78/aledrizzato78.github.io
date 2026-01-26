b5c162e2dc8828d7f18cca4d8b6810d543baf363
Aledrizzato78 <aledrizzato@gmail.com>
2026-01-26 10:15:48 -0300

    Atualiza BACKEND_URL para https://technical-deaths-affected-democrat.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2026-01-26 08:59:03
-window.BACKEND_URL = 'https://questionnaire-principle-apart-wherever.trycloudflare.com'
+// Gerado automaticamente em 2026-01-26 10:15:47
+window.BACKEND_URL = 'https://technical-deaths-affected-democrat.trycloudflare.com'
