a6b28b98af8179e6916b5b34a7ecf928e9153143
Aledrizzato78 <aledrizzato@gmail.com>
2026-01-27 10:09:51 -0300

    Atualiza BACKEND_URL para https://warning-auditor-vcr-deviation.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2026-01-27 09:13:32
-window.BACKEND_URL = 'https://checking-alt-elliott-diagnosis.trycloudflare.com'
+// Gerado automaticamente em 2026-01-27 10:09:51
+window.BACKEND_URL = 'https://warning-auditor-vcr-deviation.trycloudflare.com'
