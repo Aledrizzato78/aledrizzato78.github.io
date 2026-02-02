a7618bcdbfb908bfd69c00a31984a1b91d102694
Aledrizzato78 <aledrizzato@gmail.com>
2026-02-02 08:54:03 -0300

    Atualiza BACKEND_URL para https://research-specify-parish-beam.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2026-01-28 07:47:31
-window.BACKEND_URL = 'https://reviewer-category-quad-transmit.trycloudflare.com'
+// Gerado automaticamente em 2026-02-02 08:54:03
+window.BACKEND_URL = 'https://research-specify-parish-beam.trycloudflare.com'
