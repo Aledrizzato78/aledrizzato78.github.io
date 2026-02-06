ec4215d3e4ba4f83555ff4856bf6e427f9db0007
Aledrizzato78 <aledrizzato@gmail.com>
2026-02-06 09:03:54 -0300

    Atualiza BACKEND_URL para https://translations-input-night-stainless.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2026-02-04 09:30:44
-window.BACKEND_URL = 'https://developments-archive-identification-coding.trycloudflare.com'
+// Gerado automaticamente em 2026-02-06 09:03:54
+window.BACKEND_URL = 'https://translations-input-night-stainless.trycloudflare.com'
