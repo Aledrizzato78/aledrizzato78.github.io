5b84036b3b3b1a21f4c089dafd362e32a2030726
Aledrizzato78 <aledrizzato@gmail.com>
2026-02-09 10:25:13 -0300

    Atualiza BACKEND_URL para https://rent-broadcasting-techrepublic-manually.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2026-02-06 09:03:54
-window.BACKEND_URL = 'https://translations-input-night-stainless.trycloudflare.com'
+// Gerado automaticamente em 2026-02-09 10:25:13
+window.BACKEND_URL = 'https://rent-broadcasting-techrepublic-manually.trycloudflare.com'
