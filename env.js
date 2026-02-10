3aa6b67d629c3900067031de65d2c5f5310fca8d
Aledrizzato78 <aledrizzato@gmail.com>
2026-02-10 08:52:54 -0300

    Atualiza BACKEND_URL para https://guitar-shareware-developer-passport.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2026-02-09 10:25:13
-window.BACKEND_URL = 'https://rent-broadcasting-techrepublic-manually.trycloudflare.com'
+// Gerado automaticamente em 2026-02-10 08:52:54
+window.BACKEND_URL = 'https://guitar-shareware-developer-passport.trycloudflare.com'
