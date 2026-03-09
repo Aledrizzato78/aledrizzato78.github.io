a3d4e88acab5a181cff1d4675cdbd5802f7a10a6
Aledrizzato78 <aledrizzato@gmail.com>
2026-03-09 08:14:38 -0300

    Atualiza BACKEND_URL para https://pursue-clinics-myers-surge.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2026-03-02 09:03:12
-window.BACKEND_URL = 'https://journalist-stores-codes-caribbean.trycloudflare.com'
+// Gerado automaticamente em 2026-03-09 08:14:37
+window.BACKEND_URL = 'https://pursue-clinics-myers-surge.trycloudflare.com'
