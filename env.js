92a271796dbbac3a63790eb5e3ef717525cb2ada
Aledrizzato78 <aledrizzato@gmail.com>
2026-03-11 08:10:49 -0300

    Atualiza BACKEND_URL para https://occur-flow-because-october.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2026-03-10 08:03:33
-window.BACKEND_URL = 'https://logging-novelty-custody-plaintiff.trycloudflare.com'
+// Gerado automaticamente em 2026-03-11 08:10:48
+window.BACKEND_URL = 'https://occur-flow-because-october.trycloudflare.com'
