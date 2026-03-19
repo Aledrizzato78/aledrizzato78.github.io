c5754d8fd2cbf333354607c78c6138c28ed24047
Aledrizzato78 <aledrizzato@gmail.com>
2026-03-19 09:03:35 -0300

    Atualiza BACKEND_URL para https://what-adjustable-career-gear.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2026-03-18 08:24:46
-window.BACKEND_URL = 'https://coaches-continuously-valuation-transparency.trycloudflare.com'
+// Gerado automaticamente em 2026-03-19 09:03:34
+window.BACKEND_URL = 'https://what-adjustable-career-gear.trycloudflare.com'
