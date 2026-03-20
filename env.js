6793eba1de2bbc4fc0fc44871cd03da27045fe2d
Aledrizzato78 <aledrizzato@gmail.com>
2026-03-20 09:03:17 -0300

    Atualiza BACKEND_URL para https://luther-consistency-irrigation-surprising.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2026-03-19 09:03:34
-window.BACKEND_URL = 'https://what-adjustable-career-gear.trycloudflare.com'
+// Gerado automaticamente em 2026-03-20 09:03:17
+window.BACKEND_URL = 'https://luther-consistency-irrigation-surprising.trycloudflare.com'
