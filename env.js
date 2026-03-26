c28f98174902c6e246c54e55330309377ead1064
Aledrizzato78 <aledrizzato@gmail.com>
2026-03-26 10:02:36 -0300

    Atualiza BACKEND_URL para https://desktops-maintain-mixture-facilitate.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2026-03-20 09:03:17
-window.BACKEND_URL = 'https://luther-consistency-irrigation-surprising.trycloudflare.com'
+// Gerado automaticamente em 2026-03-26 10:02:35
+window.BACKEND_URL = 'https://desktops-maintain-mixture-facilitate.trycloudflare.com'
