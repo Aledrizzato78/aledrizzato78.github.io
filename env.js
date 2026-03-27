d221247869f5ee37ecb431d7026c68887b8c46e7
Aledrizzato78 <aledrizzato@gmail.com>
2026-03-27 09:59:42 -0300

    Atualiza BACKEND_URL para https://massachusetts-strongly-cooler-pvc.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2026-03-26 10:02:35
-window.BACKEND_URL = 'https://desktops-maintain-mixture-facilitate.trycloudflare.com'
+// Gerado automaticamente em 2026-03-27 09:59:41
+window.BACKEND_URL = 'https://massachusetts-strongly-cooler-pvc.trycloudflare.com'
