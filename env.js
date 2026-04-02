f761216f146cbe35ccea24e89af57c0957bb3c36
Aledrizzato78 <aledrizzato@gmail.com>
2026-04-02 09:43:12 -0300

    Atualiza BACKEND_URL para https://parties-representatives-contracts-planner.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2026-04-02 09:25:14
-window.BACKEND_URL = 'https://whom-predictions-achievement-seas.trycloudflare.com'
+// Gerado automaticamente em 2026-04-02 09:43:12
+window.BACKEND_URL = 'https://parties-representatives-contracts-planner.trycloudflare.com'
