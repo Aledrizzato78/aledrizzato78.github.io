b43879b3547db06f6246154025020ceed58fc07f
Aledrizzato78 <aledrizzato@gmail.com>
2026-04-13 08:19:01 -0300

    Atualiza BACKEND_URL para https://molecular-cove-rooms-cameron.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2026-04-09 08:24:43
-window.BACKEND_URL = 'https://accepted-somebody-sapphire-enhance.trycloudflare.com'
+// Gerado automaticamente em 2026-04-13 08:19:00
+window.BACKEND_URL = 'https://molecular-cove-rooms-cameron.trycloudflare.com'
