b47503df96ccb84d58216fea776c0f0e792111eb
Aledrizzato78 <aledrizzato@gmail.com>
2026-04-15 11:10:57 -0300

    Atualiza BACKEND_URL para https://chicken-museum-clearly-ultra.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2026-04-15 07:59:53
-window.BACKEND_URL = 'https://voters-strongly-tokyo-first.trycloudflare.com'
+// Gerado automaticamente em 2026-04-15 11:10:56
+window.BACKEND_URL = 'https://chicken-museum-clearly-ultra.trycloudflare.com'
