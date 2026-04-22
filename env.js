4b2912137bab37dd01a500e413e286fc861ec995
Aledrizzato78 <aledrizzato@gmail.com>
2026-04-22 09:01:32 -0300

    Atualiza BACKEND_URL para https://brad-roland-associate-motor.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2026-04-22 06:58:14
-window.BACKEND_URL = 'https://racial-tiles-tent-scale.trycloudflare.com'
+// Gerado automaticamente em 2026-04-22 09:01:32
+window.BACKEND_URL = 'https://brad-roland-associate-motor.trycloudflare.com'
