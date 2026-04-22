d0feee9467b55159f0d04834623fedac9b3e5a26
Aledrizzato78 <aledrizzato@gmail.com>
2026-04-22 15:09:58 -0300

    Atualiza BACKEND_URL para https://boxed-prompt-sms-quotations.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2026-04-22 14:44:54
-window.BACKEND_URL = 'https://florist-dock-participate-add.trycloudflare.com'
+// Gerado automaticamente em 2026-04-22 15:09:57
+window.BACKEND_URL = 'https://boxed-prompt-sms-quotations.trycloudflare.com'
