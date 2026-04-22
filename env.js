41ae6f335bbe04b778a22f739834d66f61dcad78
Aledrizzato78 <aledrizzato@gmail.com>
2026-04-22 16:35:53 -0300

    Atualiza BACKEND_URL para https://fastest-jefferson-waterproof-cloud.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2026-04-22 16:34:07
-window.BACKEND_URL = 'https://jessica-prince-market-informed.trycloudflare.com'
+// Gerado automaticamente em 2026-04-22 16:35:52
+window.BACKEND_URL = 'https://fastest-jefferson-waterproof-cloud.trycloudflare.com'
