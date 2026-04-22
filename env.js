ab1568ae936319804db0d6091ff41839683d5370
Aledrizzato78 <aledrizzato@gmail.com>
2026-04-22 16:42:06 -0300

    Atualiza BACKEND_URL para https://wireless-catalyst-trial-controls.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2026-04-22 16:35:52
-window.BACKEND_URL = 'https://fastest-jefferson-waterproof-cloud.trycloudflare.com'
+// Gerado automaticamente em 2026-04-22 16:42:05
+window.BACKEND_URL = 'https://wireless-catalyst-trial-controls.trycloudflare.com'
