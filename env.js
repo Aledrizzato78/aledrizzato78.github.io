e8c7cbcb6c54d279e538e5dde941e59d3ecc70ad
Aledrizzato78 <aledrizzato@gmail.com>
2026-04-22 14:44:55 -0300

    Atualiza BACKEND_URL para https://florist-dock-participate-add.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2026-04-22 09:01:32
-window.BACKEND_URL = 'https://brad-roland-associate-motor.trycloudflare.com'
+// Gerado automaticamente em 2026-04-22 14:44:54
+window.BACKEND_URL = 'https://florist-dock-participate-add.trycloudflare.com'
