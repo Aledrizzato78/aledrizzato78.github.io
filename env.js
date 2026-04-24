df3df899b8438bfafb6c005e3e433a3b8235209b
Aledrizzato78 <aledrizzato@gmail.com>
2026-04-24 09:02:57 -0300

    Atualiza BACKEND_URL para https://avoiding-facility-stylus-delayed.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2026-04-24 08:46:55
-window.BACKEND_URL = 'https://magnet-truck-overview-extraction.trycloudflare.com'
+// Gerado automaticamente em 2026-04-24 09:02:56
+window.BACKEND_URL = 'https://avoiding-facility-stylus-delayed.trycloudflare.com'
