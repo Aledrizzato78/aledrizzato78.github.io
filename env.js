3c92624084db3f8b168d02ad1522221488181187
Aledrizzato78 <aledrizzato@gmail.com>
2026-04-24 08:47:01 -0300

    Atualiza BACKEND_URL para https://magnet-truck-overview-extraction.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2026-04-24 08:39:23
-window.BACKEND_URL = 'https://photographers-donor-caution-architect.trycloudflare.com'
+// Gerado automaticamente em 2026-04-24 08:46:55
+window.BACKEND_URL = 'https://magnet-truck-overview-extraction.trycloudflare.com'
