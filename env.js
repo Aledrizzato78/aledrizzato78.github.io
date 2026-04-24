6a8a6df68871c6679aa034eee16e4d904ae331b4
Aledrizzato78 <aledrizzato@gmail.com>
2026-04-24 09:07:20 -0300

    Atualiza BACKEND_URL para https://worldcat-paul-ships-furnished.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2026-04-24 09:02:56
-window.BACKEND_URL = 'https://avoiding-facility-stylus-delayed.trycloudflare.com'
+// Gerado automaticamente em 2026-04-24 09:07:19
+window.BACKEND_URL = 'https://worldcat-paul-ships-furnished.trycloudflare.com'
