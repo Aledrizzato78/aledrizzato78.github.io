976f3b1ad8c1d7d09b03550ec8fcc79096a7c801
Aledrizzato78 <aledrizzato@gmail.com>
2026-04-27 09:40:18 -0300

    Atualiza BACKEND_URL para https://concord-thumbzilla-com-protected.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2026-04-27 09:36:47
-window.BACKEND_URL = 'https://computation-library-frequently-enter.trycloudflare.com'
+// Gerado automaticamente em 2026-04-27 09:40:18
+window.BACKEND_URL = 'https://concord-thumbzilla-com-protected.trycloudflare.com'
