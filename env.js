2fd547b26b0cb66f1aceb1611a6bc6224009e206
Aledrizzato78 <aledrizzato@gmail.com>
2026-05-04 08:03:16 -0300

    Atualiza BACKEND_URL para https://actress-disclaimer-mail-fires.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2026-04-30 06:17:34
-window.BACKEND_URL = 'https://cheap-enlarge-encoding-examine.trycloudflare.com'
+// Gerado automaticamente em 2026-05-04 08:03:14
+window.BACKEND_URL = 'https://actress-disclaimer-mail-fires.trycloudflare.com'
