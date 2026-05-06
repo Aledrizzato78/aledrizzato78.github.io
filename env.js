138742dfa8b318eadde7651d98523e6ec9eaf0ae
Aledrizzato78 <aledrizzato@gmail.com>
2026-05-06 06:37:37 -0300

    Atualiza BACKEND_URL para https://designers-federation-pst-instruments.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2026-05-06 06:26:56
-window.BACKEND_URL = 'https://matter-suburban-losing-inserted.trycloudflare.com'
+// Gerado automaticamente em 2026-05-06 06:37:36
+window.BACKEND_URL = 'https://designers-federation-pst-instruments.trycloudflare.com'
