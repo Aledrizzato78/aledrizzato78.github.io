709ae035771d6a056c78d56c277187ed582db80b
Aledrizzato78 <aledrizzato@gmail.com>
2026-05-25 09:32:05 -0300

    Atualiza BACKEND_URL para https://designer-any-calculators-bids.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2026-05-25 09:21:53
-window.BACKEND_URL = 'https://hero-raid-mobiles-author.trycloudflare.com'
+// Gerado automaticamente em 2026-05-25 09:32:04
+window.BACKEND_URL = 'https://designer-any-calculators-bids.trycloudflare.com'
