493139ecba0748645f7b7e29157cb7eabff53403
Aledrizzato78 <aledrizzato@gmail.com>
2026-05-25 09:49:19 -0300

    Atualiza BACKEND_URL para https://trade-appreciated-guy-blocked.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2026-05-25 09:41:15
-window.BACKEND_URL = 'https://grammar-ranging-pair-minimize.trycloudflare.com'
+// Gerado automaticamente em 2026-05-25 09:49:18
+window.BACKEND_URL = 'https://trade-appreciated-guy-blocked.trycloudflare.com'
