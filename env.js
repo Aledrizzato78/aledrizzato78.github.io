9c75e096cf27dd3f97350a2fb1f3a2ce90f46d70
Aledrizzato78 <aledrizzato@gmail.com>
2026-05-26 09:11:37 -0300

    Atualiza BACKEND_URL para https://paper-national-daughter-habits.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2026-05-26 08:29:30
-window.BACKEND_URL = 'https://hang-powerpoint-steady-proc.trycloudflare.com'
+// Gerado automaticamente em 2026-05-26 09:11:35
+window.BACKEND_URL = 'https://paper-national-daughter-habits.trycloudflare.com'
