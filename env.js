5883f1305af7f8506ff4a6295292ecc09d7c9851
Aledrizzato78 <aledrizzato@gmail.com>
2026-05-26 08:29:33 -0300

    Atualiza BACKEND_URL para https://hang-powerpoint-steady-proc.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2026-05-25 09:49:18
-window.BACKEND_URL = 'https://trade-appreciated-guy-blocked.trycloudflare.com'
+// Gerado automaticamente em 2026-05-26 08:29:30
+window.BACKEND_URL = 'https://hang-powerpoint-steady-proc.trycloudflare.com'
