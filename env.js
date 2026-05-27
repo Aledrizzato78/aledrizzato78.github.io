89d3eca425d1e9bd19b064c1af1e800d2a467358
Aledrizzato78 <aledrizzato@gmail.com>
2026-05-27 08:57:21 -0300

    Atualiza BACKEND_URL para https://lock-hockey-did-test.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2026-05-26 09:28:57
-window.BACKEND_URL = 'https://arbitration-convention-mesh-grew.trycloudflare.com'
+// Gerado automaticamente em 2026-05-27 08:57:20
+window.BACKEND_URL = 'https://lock-hockey-did-test.trycloudflare.com'
