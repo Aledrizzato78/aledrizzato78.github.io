682ad86b8585dc91ab0d7fc938433a28e29a621b
Aledrizzato78 <aledrizzato@gmail.com>
2026-05-29 07:00:15 -0300

    Atualiza BACKEND_URL para https://theory-producer-allowance-pet.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2026-05-28 08:11:52
-window.BACKEND_URL = 'https://prepared-implementation-associates-employ.trycloudflare.com'
+// Gerado automaticamente em 2026-05-29 07:00:14
+window.BACKEND_URL = 'https://theory-producer-allowance-pet.trycloudflare.com'
