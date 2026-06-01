85cef81b441095656d2d249b4424e6f924925d58
Aledrizzato78 <aledrizzato@gmail.com>
2026-06-01 06:23:54 -0300

    Atualiza BACKEND_URL para https://agents-high-inf-bias.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2026-05-29 07:00:14
-window.BACKEND_URL = 'https://theory-producer-allowance-pet.trycloudflare.com'
+// Gerado automaticamente em 2026-06-01 06:23:53
+window.BACKEND_URL = 'https://agents-high-inf-bias.trycloudflare.com'
