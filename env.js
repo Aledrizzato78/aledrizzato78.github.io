92a88e20da2c90df0b803f664a4d50301288cfd8
Aledrizzato78 <aledrizzato@gmail.com>
2026-06-08 09:28:45 -0300

    Atualiza BACKEND_URL para https://visual-grey-browser-process.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2026-06-03 08:24:50
-window.BACKEND_URL = 'https://moses-literally-tribes-know.trycloudflare.com'
+// Gerado automaticamente em 2026-06-08 09:28:40
+window.BACKEND_URL = 'https://visual-grey-browser-process.trycloudflare.com'
