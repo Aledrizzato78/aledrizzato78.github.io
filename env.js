c633e12625e35dcbf8e3963a12b849f1a6bd4c28
Aledrizzato78 <aledrizzato@gmail.com>
2026-06-09 08:12:01 -0300

    Atualiza BACKEND_URL para https://contacting-constitutional-passed-other.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2026-06-08 09:28:40
-window.BACKEND_URL = 'https://visual-grey-browser-process.trycloudflare.com'
+// Gerado automaticamente em 2026-06-09 08:12:00
+window.BACKEND_URL = 'https://contacting-constitutional-passed-other.trycloudflare.com'
