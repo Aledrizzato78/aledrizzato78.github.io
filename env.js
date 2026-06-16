e4372926812b2d4cf63a68762f01f3a984c2f9e7
Aledrizzato78 <aledrizzato@gmail.com>
2026-06-16 07:29:32 -0300

    Atualiza BACKEND_URL para https://responsible-students-calculation-montgomery.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2026-06-15 07:36:17
-window.BACKEND_URL = 'https://tale-mostly-viewed-basket.trycloudflare.com'
+// Gerado automaticamente em 2026-06-16 07:29:31
+window.BACKEND_URL = 'https://responsible-students-calculation-montgomery.trycloudflare.com'
