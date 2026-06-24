03d21c93b83938abeae4a99929286034d0ec14ee
Aledrizzato78 <aledrizzato@gmail.com>
2026-06-24 06:11:19 -0300

    Atualiza BACKEND_URL para https://buffalo-tables-indie-toys.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2026-06-23 06:15:50
-window.BACKEND_URL = 'https://virtue-consequences-africa-comply.trycloudflare.com'
+// Gerado automaticamente em 2026-06-24 06:11:18
+window.BACKEND_URL = 'https://buffalo-tables-indie-toys.trycloudflare.com'
