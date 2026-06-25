d389725cb8fe82e4a729cc2e41f452945750349e
Aledrizzato78 <aledrizzato@gmail.com>
2026-06-25 07:01:05 -0300

    Atualiza BACKEND_URL para https://tee-tension-seas-piece.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2026-06-24 06:11:18
-window.BACKEND_URL = 'https://buffalo-tables-indie-toys.trycloudflare.com'
+// Gerado automaticamente em 2026-06-25 07:01:04
+window.BACKEND_URL = 'https://tee-tension-seas-piece.trycloudflare.com'
