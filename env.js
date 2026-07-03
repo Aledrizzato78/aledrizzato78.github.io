254353207de65e82bedce8330411bd2e195e6112
Aledrizzato78 <aledrizzato@gmail.com>
2026-07-03 09:54:18 -0300

    Atualiza BACKEND_URL para https://avon-united-alice-stage.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2026-07-03 06:02:10
-window.BACKEND_URL = 'https://figured-gmbh-spectrum-acting.trycloudflare.com'
+// Gerado automaticamente em 2026-07-03 09:54:17
+window.BACKEND_URL = 'https://avon-united-alice-stage.trycloudflare.com'
