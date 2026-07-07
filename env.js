658dc67bd0167a8b07d389332b4df95616925c8d
Aledrizzato78 <aledrizzato@gmail.com>
2026-07-07 07:10:56 -0300

    Atualiza BACKEND_URL para https://agents-allowed-spoken-interracial.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2026-07-06 06:52:45
-window.BACKEND_URL = 'https://definition-other-afterwards-artificial.trycloudflare.com'
+// Gerado automaticamente em 2026-07-07 07:10:53
+window.BACKEND_URL = 'https://agents-allowed-spoken-interracial.trycloudflare.com'
