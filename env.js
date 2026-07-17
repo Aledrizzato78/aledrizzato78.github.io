357a114e388ccddc1db32de666ef7a72e0cf1cda
Aledrizzato78 <aledrizzato@gmail.com>
2026-07-17 05:20:40 -0300

    Atualiza BACKEND_URL para https://valves-marks-mono-msgid.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2026-07-16 08:06:05
-window.BACKEND_URL = 'https://mortgages-working-leader-stream.trycloudflare.com'
+// Gerado automaticamente em 2026-07-17 05:20:35
+window.BACKEND_URL = 'https://valves-marks-mono-msgid.trycloudflare.com'
