6e4bcfd3a5c256d2303be0e782b7bba991352392
Aledrizzato78 <aledrizzato@gmail.com>
2026-07-22 06:17:34 -0300

    Atualiza BACKEND_URL para https://specialties-tile-brussels-genre.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2026-07-21 06:23:25
-window.BACKEND_URL = 'https://vertex-largest-bachelor-based.trycloudflare.com'
+// Gerado automaticamente em 2026-07-22 06:17:33
+window.BACKEND_URL = 'https://specialties-tile-brussels-genre.trycloudflare.com'
