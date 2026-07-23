b4bf158d549362c148ae97444303129fce3e25dd
Aledrizzato78 <aledrizzato@gmail.com>
2026-07-23 07:29:02 -0300

    Atualiza BACKEND_URL para https://displayed-dates-nominations-electricity.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2026-07-22 06:17:33
-window.BACKEND_URL = 'https://specialties-tile-brussels-genre.trycloudflare.com'
+// Gerado automaticamente em 2026-07-23 07:29:01
+window.BACKEND_URL = 'https://displayed-dates-nominations-electricity.trycloudflare.com'
