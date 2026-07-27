8a1bae141f5040da618a5c13462263f1177e4bec
Aledrizzato78 <aledrizzato@gmail.com>
2026-07-27 08:34:14 -0300

    Atualiza BACKEND_URL para https://refresh-vids-flavor-correctly.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2026-07-27 07:41:32
-window.BACKEND_URL = 'https://amount-editor-instrumental-mix.trycloudflare.com'
+// Gerado automaticamente em 2026-07-27 08:34:13
+window.BACKEND_URL = 'https://refresh-vids-flavor-correctly.trycloudflare.com'
