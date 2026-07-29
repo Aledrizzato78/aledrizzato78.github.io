8f1e118877224ad8376eea4b08f504bf96dd8018
Aledrizzato78 <aledrizzato@gmail.com>
2026-07-29 06:34:24 -0300

    Atualiza BACKEND_URL para https://pursue-hide-universities-auburn.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2026-07-28 07:24:33
-window.BACKEND_URL = 'https://hook-buzz-trainers-commerce.trycloudflare.com'
+// Gerado automaticamente em 2026-07-29 06:34:23
+window.BACKEND_URL = 'https://pursue-hide-universities-auburn.trycloudflare.com'
