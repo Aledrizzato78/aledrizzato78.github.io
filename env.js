e50973791037d532b8ed6810bd3dafedb55b38a2
Aledrizzato78 <aledrizzato@gmail.com>
2026-07-31 06:31:41 -0300

    Atualiza BACKEND_URL para https://economy-dependence-tray-kitchen.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2026-07-30 07:48:24
-window.BACKEND_URL = 'https://cinema-van-icq-ooo.trycloudflare.com'
+// Gerado automaticamente em 2026-07-31 06:31:36
+window.BACKEND_URL = 'https://economy-dependence-tray-kitchen.trycloudflare.com'
