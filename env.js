8dc1ce6e483c81e6ca53e82c22b80d79a387633d
Aledrizzato78 <aledrizzato@gmail.com>
2026-08-03 06:36:44 -0300

    Atualiza BACKEND_URL para https://jake-kate-masters-cold.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2026-07-31 06:31:36
-window.BACKEND_URL = 'https://economy-dependence-tray-kitchen.trycloudflare.com'
+// Gerado automaticamente em 2026-08-03 06:36:35
+window.BACKEND_URL = 'https://jake-kate-masters-cold.trycloudflare.com'
