59362fab09fc80c82b1eb5562ef715534bbdeb6b
Aledrizzato78 <aledrizzato@gmail.com>
2026-08-04 05:12:39 -0300

    Atualiza BACKEND_URL para https://licensing-idle-denver-climbing.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2026-08-03 11:34:08
-window.BACKEND_URL = 'https://terms-enhancement-spine-nissan.trycloudflare.com'
+// Gerado automaticamente em 2026-08-04 05:12:38
+window.BACKEND_URL = 'https://licensing-idle-denver-climbing.trycloudflare.com'
