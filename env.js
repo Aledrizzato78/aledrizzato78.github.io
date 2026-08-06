9923b3f369452665e6784afdd3f73eca722173d8
Aledrizzato78 <aledrizzato@gmail.com>
2026-08-06 08:34:11 -0300

    Atualiza BACKEND_URL para https://sauce-appears-locked-carolina.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2026-08-06 08:05:55
-window.BACKEND_URL = 'https://mic-musician-fri-attending.trycloudflare.com'
+// Gerado automaticamente em 2026-08-06 08:34:10
+window.BACKEND_URL = 'https://sauce-appears-locked-carolina.trycloudflare.com'
