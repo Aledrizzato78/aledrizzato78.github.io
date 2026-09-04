1fc31e3dd95f0b98fa8dbf4836e417372748ecfc
Aledrizzato78 <aledrizzato@gmail.com>
2026-09-04 05:34:17 -0300

    Atualiza BACKEND_URL para https://dealer-highlighted-harold-makes.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2026-09-03 05:39:49
-window.BACKEND_URL = 'https://surrounding-viewer-entitled-resistant.trycloudflare.com'
+// Gerado automaticamente em 2026-09-04 05:34:15
+window.BACKEND_URL = 'https://dealer-highlighted-harold-makes.trycloudflare.com'
