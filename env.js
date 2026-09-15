c5cb54ffb2cc3c59ae169693c685e51276f76e8f
Aledrizzato78 <aledrizzato@gmail.com>
2026-09-15 05:28:15 -0300

    Atualiza BACKEND_URL para https://embassy-interracial-corresponding-drove.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2026-09-14 07:15:36
-window.BACKEND_URL = 'https://guardian-remarks-generated-prove.trycloudflare.com'
+// Gerado automaticamente em 2026-09-15 05:28:13
+window.BACKEND_URL = 'https://embassy-interracial-corresponding-drove.trycloudflare.com'
