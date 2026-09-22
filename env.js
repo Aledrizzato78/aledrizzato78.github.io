4e174d797cd988ed165117cdb250865204fe6580
Aledrizzato78 <aledrizzato@gmail.com>
2026-09-22 07:02:22 -0300

    Atualiza BACKEND_URL para https://through-optimization-latina-proposals.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2026-09-21 06:17:19
-window.BACKEND_URL = 'https://rise-success-house-federal.trycloudflare.com'
+// Gerado automaticamente em 2026-09-22 07:02:21
+window.BACKEND_URL = 'https://through-optimization-latina-proposals.trycloudflare.com'
