704b5d2844bfb335c9cda20b28e6556b6dc742c1
Aledrizzato78 <aledrizzato@gmail.com>
2026-09-24 07:08:36 -0300

    Atualiza BACKEND_URL para https://intervention-anytime-engagement-arguments.trycloudflare.com

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// Gerado automaticamente em 2026-09-23 07:02:41
-window.BACKEND_URL = 'https://early-suits-story-apparent.trycloudflare.com'
+// Gerado automaticamente em 2026-09-24 07:08:35
+window.BACKEND_URL = 'https://intervention-anytime-engagement-arguments.trycloudflare.com'
